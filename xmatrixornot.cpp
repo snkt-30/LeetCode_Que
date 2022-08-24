99d879e25e30c46aaa6bdc9791fad36906ee91d2
Sanket <sanketkhonde30@gmail.com>
2022-08-24 14:51:36 +0530

    check the matrix is valid or not

---
+++ xmatrixornot.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool checkXMatrix(vector<vector<int>> &grid)
+{
+    int n=grid.size();
+    int m=grid[0].size();
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if((i==j || (i+j)==n-1) and grid[i][j]!=0)
+            {
+                return false;
+            }
+            if((i!=j || (i+j)!=n-1)and grid[i][j]==0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+int main()
+{
+    
+}
