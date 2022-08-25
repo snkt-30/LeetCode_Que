e9917fcc5fb0f05ff11774218f51eda08a530ad4
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2022-08-25 17:57:32 +0530

    Update countingelementintwoarrays.cpp

--- countingelementintwoarrays.cpp
+++ countingelementintwoarrays.cpp
@@ -1,10 +1,10 @@
 #include"bits/stdc++.h"
 using namespace std;
 
-
-
+// 1<= N<=1e5
   vector<int> countEleLessThanOrEqual(int arr1[], int arr2[], int m, int n){
     
+    // O(N);
     vector<int> count(100001,0);
 
     for(int i=0;i<n;i++)
@@ -27,6 +27,18 @@ using namespace std;
     }
     
     return ans;
+    
+    // second approach 
+//      vector<int> countEleLessThanOrEqual(int arr1[], int arr2[], int m, int n) {
+        
+//         vector<int> ans;
+//         sort(arr2, arr2+n);
+//         for(int i=0; i<m; i++) {
+//             int num = upper_bound(arr2, arr2 + n, arr1[i]) - arr2;
+//             ans.push_back(num);
+//         }
+//         return ans;
+//     }
     
     }
 int main()
