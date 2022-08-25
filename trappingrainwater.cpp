0bd1f9af0c3989da1757f8e20c5ce1425f86e046
Sanket <sanketkhonde30@gmail.com>
2022-08-25 18:38:28 +0530

    changes to the trapping rain water problem

--- trappingrainwater.cpp
+++ trappingrainwater.cpp
@@ -1,52 +1,26 @@
 #include "bits/stdc++.h"
 using namespace std;
-
-int trap(vector<int> &height)
+   int trap(vector<int> &height)
 {
+       int n=height.size();
+    vector<int> left(n,0), right(n,0);
 
-    int currmx=INT_MIN;
-    int mxwater=0;
-    int st=height[0];
-
-    for(int i=1;i<height.size();i++)
-    {
-        if(height[i]<st)
-        {
-            currmx+=(st-height[i]);
-        }
-        else if(st<height[i])
-        {
-            mxwater=max(mxwater,currmx);
-            st=height[i];
-        }
-    }
-    return mxwater;
-}
-
-int Trapping_Rain_Water(vector<int> &height)
-{
-    vector<int> left(height.size(),0), right(height.size(),0);
-
-     left[0]=height[0];
-    for(int i=1;i<height.size();i++)
+    left[0]=height[0];
+    for(int i=1;i<n;i++)
     {
         left[i]=max(left[i-1],height[i]);
     }
-    
-    for(int i=height.size()-1;i>=0;i--)
+    right[n-1]=height[n-1];
+    for(int i=n-2;i>=0;i--)
     {
         right[i]=max(right[i+1],height[i]);
     }
-    
     int ans=0;
     for(int i=0;i<height.size();i++)
     {
         ans+=(min(left[i],right[i]))-height[i];
     }
-
     return ans;
-
-
 }
 int main()
 {
