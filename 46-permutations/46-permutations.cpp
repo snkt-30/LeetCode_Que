8d9c6c70ab37bc8be6728ca9f5a0f2e549dbd7a6
1_Xor_0 <84696262+snkt-30@users.noreply.github.com>
2022-09-15 09:16:22 +0530

    Time: 7 ms (41.30%), Space: 7.8 MB (54.82%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -2,26 +2,26 @@ class Solution
 {
     public:
 
-        void gen_permute(vector<int> &ds, vector<int> &nums, vector< vector< int>> &ans, int freq[])
-        {
-            if (ds.size() == nums.size())
-            {
-                ans.push_back(ds);
-                return;
-            }
-
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (!freq[i])
-                {
-                    ds.push_back(nums[i]);
-                    freq[i] = 1;
-                    gen_permute(ds, nums, ans, freq);
-                    ds.pop_back();
-                    freq[i] = 0;
-                }
-            }
-        }
+       void gen_permute(vector<int>&ds,vector<int>&ar,vector<vector<int>>&ans,int freq[])
+       {
+           if(ds.size()==ar.size())
+           {
+               ans.push_back(ds);
+               return;
+           }
+           
+           for(int i=0;i<ar.size();i++)
+           {
+               if(!freq[i])
+               {
+                          ds.push_back(ar[i]);
+                     freq[i]=1;
+                   gen_permute(ds,ar,ans,freq);
+                   ds.pop_back();
+                   freq[i]=0;
+               }
+           }
+       }
     vector<vector < int>> permute(vector<int> &nums)
     {
 
