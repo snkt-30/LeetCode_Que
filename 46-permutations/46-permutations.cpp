182b7896130a2ec1f8550b477ddf7bd9a1d9519d
1_Xor_0 <84696262+snkt-30@users.noreply.github.com>
2022-09-16 09:35:43 +0530

    Time: 5 ms (51.90%), Space: 7.6 MB (91.55%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -2,7 +2,7 @@ class Solution
 {
     public:
 
-        void gen_permute(int ind,vector<int> &ds, vector<int> &ar, vector< vector< int>> &ans)
+        void gen_permute(int ind, vector<int> &ar, vector< vector< int>> &ans)
         {
             if(ind==ar.size())
             {
@@ -12,7 +12,7 @@ class Solution
             for(int i=ind;i<ar.size();i++)
             {
                 swap(ar[i],ar[ind]);
-                gen_permute(ind+1,ds,ar,ans);
+                gen_permute(ind+1,ar,ans);
                 swap(ar[i],ar[ind]);
             }
         }
@@ -20,11 +20,11 @@ class Solution
     {
 
         vector<vector < int>> ans;
-        vector<int> ds;
+        // vector<int> ds;
         // int freq[nums.size()];
 
 
-        gen_permute(0,ds, nums, ans);
+        gen_permute(0,nums, ans);
 
         return ans;
     }
