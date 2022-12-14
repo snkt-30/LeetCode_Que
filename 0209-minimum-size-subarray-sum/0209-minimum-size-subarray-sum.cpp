ac5ce3d41411811432c18aeb805feeb7c94e33af
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2022-12-14 19:48:22 +0530

    Time: 44 ms (87.35%), Space: 28.2 MB (54.28%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
-        
-        int len=100001;
-        
-        int st_ind=0,end_ind=0;
-        
-        int sum=0;
-        
-        while(end_ind<nums.size())
-        {
-            sum+=nums[end_ind];
-            
-            if(sum<target)
-              end_ind++;
-            else if(sum>=target)
-            {
-                while(sum>=target and st_ind<=end_ind)
-                {
-                    int curr=end_ind-st_ind+1;
-                    if(len>curr)
-                    {
-                        len=curr;
-                    }
-                    sum-=nums[st_ind];
-                    st_ind++;
-                }
-                end_ind++;
-            }
-        }
-        
-        return len==100001?0:len;
+     
+    int len = 100001;
+
+	int st_ind = 0, end_ind = 0;
+
+	int sum = 0;
+
+	while (end_ind < nums.size())
+	{
+		sum += nums[end_ind];
+
+		if (sum < target)
+			end_ind++;
+		else if (sum >= target)
+		{
+			while (sum >= target and st_ind <= end_ind)
+			{
+				int curr = end_ind - st_ind + 1;
+				if (len > curr)
+				{
+					len = curr;
+				}
+				sum -= nums[st_ind];
+				st_ind++;
+			}
+			end_ind++;
+		}
+	}
+
+	return len == 100001 ? 0 : len;
     }
 };
