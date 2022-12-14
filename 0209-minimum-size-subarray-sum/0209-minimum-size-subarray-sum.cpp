538ef7e4fb6bc189a4eec7b88e2a95c2dd614519
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2022-12-14 20:03:10 +0530

    Time: 42 ms (90.53%), Space: 29.1 MB (6.61%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-     
+    int minSubArrayLen(int s, vector<int>& nums) {
+   /*  
+     time Complexity : O(N)
     int len = 100001;
 
 	int st_ind = 0, end_ind = 0;
@@ -31,5 +32,29 @@ public:
 	}
 
 	return len == 100001 ? 0 : len;
+    
+    */
+        
+  // Time Complexity O(log(N))
+        
+        int n=nums.size();
+        
+        vector<int> v(n+1,0);
+        
+        int len=INT_MAX;
+        
+        for(int i=1;i<=n;i++)
+        {
+            v[i]=v[i-1]+nums[i-1];
+        }
+        
+        for(int i=n;i>=0 and v[i]>=s;i--)
+        {
+            int ind=upper_bound(v.begin(),v.end(),v[i]-s)-v.begin();
+            len=min(len,i-ind+1);
+            
+        }
+        
+        return len==INT_MAX?0:len;
     }
 };
