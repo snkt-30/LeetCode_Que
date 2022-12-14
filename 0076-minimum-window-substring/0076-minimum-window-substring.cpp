0776ba7e9c21294c88ee2cd7eb72fab8119c3d5b
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2022-12-14 19:09:10 +0530

    Time: 100 ms (15.48%), Space: 21 MB (5.00%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+    unordered_map<char,int> mp;
+        
+        int ns=s.length();
+        int nt=t.length();
+        
+        int cnt=0;
+        
+        for(int i=0;i<nt;i++)
+        {
+            if(mp.count(t[i])==0)
+                cnt++;
+            mp[t[i]]++;
+        }
+        
+        int in=0,ls=0;
+        
+        set<pair<int,int>> st;
+        queue<char> q;
+        while(ls<ns)
+        {
+            // precomputation
+            q.push(s[ls]);
+            if(mp.count(s[ls]))
+            {
+                mp[s[ls]]--;
+                if(mp[s[ls]]==0)
+                    cnt--;
+            }
+            
+            
+            if(cnt!=0)
+                ls++;
+            
+            // sataisfied cond
+            if(cnt==0)
+            {
+                while(cnt==0)
+                {
+                    st.insert(make_pair(ls-in+1,in));
+                    q.pop();
+                    if(mp.count(s[in]))
+                    {
+                        if(mp[s[in]]==0)
+                            cnt++;
+                        mp[s[in]]++;
+                    }
+                    in++;                  
+                }
+                ls++;
+                          
+            }   
+        }
+        
+        pair<int,int> temp=*st.begin();
+        st.erase(temp);
+        int st_ind=temp.second;
+        int sz=temp.first;
+        
+        string res="";
+        
+        while(sz)
+        {
+            res.push_back(s[st_ind]);
+            st_ind++;
+            sz--;
+        }
+        return res;
+    }
+};
