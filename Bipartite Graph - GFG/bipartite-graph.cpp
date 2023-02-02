af85c6dff495682cf7580317c3470ba4b594bfa5
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-02 15:27:34 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,81 +4,43 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    
-    // private:
-    
-    // bool check(int st,int V,vector<int>ajd[],int col[])
-    // {
-    //     queue<int>bfs;
-        
-    //     bfs.push(st);
-        
-    //     while(!bfs.empty())
-    //     {
-    //         int node=bfs.front();
-    //         bfs.pop();
-            
-    //         for(auto it:ajd[node])
-    //         {
-    //             if(col[it]==-1)
-    //             {
-    //                 bfs.push(it);
-    //                 col[it]=!col[node];
-    //             }
-    //             else
-    //             {
-    //                 if(col[it]==col[node])
-    //                   return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
 public:
-	bool isBipartite(int V, vector<int>ajd[]){
-	   // // Code here
-	   
-	   int col[V];
-	   
-	   
-	   for(int i=0;i<V;i++)
-	   {
-	       col[i]=-1;
-	   }
-	   
-	   
-	   for(int i=0;i<V;i++)
-	   {
-	       if(col[i]==-1)
-	       {
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	    
+	 vector<int>color(V+1,-1);
+	 
+	 
+	 for(int i=0;i<V;i++)
+	 {
+	     if(color[i]==-1)
+	     {
 	         queue<int>bfs;
-        
-             bfs.push(i);
-        
-        while(!bfs.empty())
-        {
-            int node=bfs.front();
-            bfs.pop();
-            
-            for(auto it:ajd[node])
-            {
-                if(col[it]==-1)
-                {
-                    bfs.push(it);
-                    col[it]=!col[node];
-                }
-                else
-                {
-                    if(col[it]==col[node])
-                      return false;
-                }
-            }
-        }
-	       }
-	   }
-	   return true;
-	}
-
+	         bfs.push(i);
+	         color[i]=0;
+	         
+	         while(!bfs.empty())
+	         {
+	             int node=bfs.front();
+	             bfs.pop();
+	             
+	             for(auto x:adj[node])
+	             {
+	                 if(color[x]==-1)
+	                 {
+	                     color[x]=!color[node];
+	                     bfs.push(x);
+	                 }
+	                 else
+	                 {
+	                     if(color[node]==color[x])return false;
+	                 }
+	             }
+	         }
+	     }
+	 }
+	 return true;
+  }
 };
 
 //{ Driver Code Starts.
