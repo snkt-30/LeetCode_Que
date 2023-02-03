5bda1d84f8633e98d71c19f076ebaae8231180af
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-03 14:41:34 +0530

    Time: 6 ms (87.45%), Space: 9.1 MB (49.36%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,78 @@
+class DSU{
+    
+    private:
+    vector<int>parent,size;
+    
+    public:
+     DSU(int n)
+    {
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        
+        for(int i=1;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+    }
+    
+    int find_par(int node)
+    {
+        if(node==parent[node])
+            return node;
+        
+        return parent[node]=find_par(parent[node]);
+    }
+    
+    void Union(int a,int b)
+    {
+        int par_a=find_par(a);
+        int par_b=find_par(b);
+        
+        if(par_a != par_b)
+        {
+            if(size[par_a]<size[par_b])
+                swap(par_a,par_b);
+            
+           parent[par_b]=par_a;
+            size[par_a]+=size[par_b];
+        }
+    }
+    
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+         int n=edges.size();
+        
+        DSU obj(n);
+        
+        vector<int>ans;
+        
+        for(auto x:edges)
+        {
+            int u=x[0];
+            int v=x[1];
+            
+            int par_u=obj.find_par(u);
+            int par_v=obj.find_par(v);
+            
+            if(par_u!=par_v)
+            {
+                obj.Union(u,v);
+            }
+            else
+            {
+               ans=x;
+                break;
+            }
+        }
+        
+        return ans;
+        
+        
+        
+        
+    }
+};
