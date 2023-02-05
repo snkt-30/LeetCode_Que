7d4c3bc3660201468003a1c206bf019c68d8691c
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-05 18:05:33 +0530

    Time: 47 ms (43.56%), Space: 19.2 MB (13.45%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,62 @@
+class Solution {
+    
+    private:
+    static bool isvalid(int row,int col,int n,int m)
+    {
+        return (row>=0 and row<n and col>=0 and col<m);
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        int dx[4]={-1,1,0,0};
+        int dy[4]={0,0,-1,1};
+        
+        int cnt=0;
+        
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1' and !vis[i][j])
+                {
+                    cnt++;
+                    queue<pair<int,int>> bfs;
+                    
+                    bfs.push(make_pair(i,j));
+                    vis[i][j]=1;
+                    
+                    while(!bfs.empty())
+                    {
+                        pair<int,int> node=bfs.front();
+                        bfs.pop();
+                        
+                        int row=node.first;
+                        int col=node.second;
+                        
+                        for(int del=0;del<4;del++)
+                        {
+                            int nrow=row+dx[del];
+                            int ncol=col+dy[del];
+                            
+                            if(isvalid(nrow,ncol,n,m) and !vis[nrow][ncol] and grid[nrow][ncol]==
+                              '1')
+                            {
+                                vis[nrow][ncol]=1;
+                                bfs.push(make_pair(nrow,ncol));
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return cnt;
+        
+    }
+};
