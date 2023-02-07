3c4f13ec4cad27366fce728905923543d69f5b7b
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-07 16:03:57 +0530

    Time: 189 ms (63.16%), Space: 73.3 MB (23.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,57 +1,56 @@
-class Solution {
-public:
-    int totalFruit(vector<int>& f) {
-        
-        
-        int ans=0;
-        
-        int l=0,r=0;
-        
-        int n=f.size();
-             
-        unordered_map<int,int>mp;      
-        
-        int cnt=0;
-
-        while(r<n)
+class Solution
+{
+    public:
+        int totalFruit(vector<int> &f)
         {
-            // calculation
-            
-            if(mp[f[r]])
-            {
-                mp[f[r]]++;
-            }
-            else
-            {
-                mp[f[r]]++;
-                cnt++;
-            }
-                    
-            // when cnt is < 2
 
-             if(cnt<=2)
+            int ans = 0;
+
+            int l = 0, r = 0;
+
+            int n = f.size();
+
+            unordered_map<int, int> mp;
+
+            int cnt = 0;
+
+            while (r < n)
             {
-                int curr=r-l+1;
-                
-                ans=max(ans,curr);
-                r++;
-            }
-            else
-            {
-                while(cnt>2)
+               	// calculation
+
+                if (mp[f[r]])
                 {
-                    mp[f[l]]--;
-                    if(mp[f[l]]==0)
-                        cnt--;
-                    l++;
+                    mp[f[r]]++;
                 }
-                r++;
-            }
-            
-            // cout<<cnt<<" "<<ans<<endl;
+                else
+                {
+                    mp[f[r]]++;
+                    cnt++;
+                }
 
+               	// when cnt is < 2
+
+                if (cnt <= 2)
+                {
+                    int curr = r - l + 1;
+
+                    ans = max(ans, curr);
+                    r++;
+                }
+                else
+                {
+                    while (cnt > 2)
+                    {
+                        mp[f[l]]--;
+                        if (mp[f[l]] == 0)
+                            cnt--;
+                        l++;
+                    }
+                    r++;
+                }
+               	// cout<<cnt<<" "<<ans<<endl;
+            }
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
