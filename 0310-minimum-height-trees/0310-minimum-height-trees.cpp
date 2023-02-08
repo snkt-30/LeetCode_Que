8819136b04e287407e7f0a720a32a04c71cf00fd
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-08 17:11:17 +0530

    Time: 166 ms (81.22%), Space: 54.6 MB (60.90%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,104 +1,53 @@
+#define pb emplace_back
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& e) {
         
-        vector<int> adj[n];
+        vector<int>ind(n);
+        vector<int>adj[n];
         
-        vector<int>indegeree(n);
+        if(n==1)
+            return {0};
         
-        for(int i=0;i<edges.size();i++)
+        for(auto x:e)
         {
-            int u=edges[i][0];
-            int v=edges[i][1];
+            int u=x[0];
+            int v=x[1];
             
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            indegeree[u]++;
-            indegeree[v]++;
+            adj[u].pb(v);
+            adj[v].pb(u);
+            ind[v]++;
+            ind[u]++;
         }
         
-        queue<int>q;
+        vector<int>ans;
+        queue<int>bfs;
         
         for(int i=0;i<n;i++)
         {
-            if(indegeree[i]==1)
-            {
-                q.push(i);
-            }
+            if(ind[i]==1)
+                bfs.push(i);
         }
-        vector<int>ans;
         
-        while(!q.empty())
+        while(!bfs.empty())
         {
-            int s=q.size();
+            int sz=bfs.size();
             ans.clear();
             
-            for(int i=0;i<s;i++)
+            for(int i=0;i<sz;i++)
             {
-                int  node=q.front();
-                q.pop();
+                int node=bfs.front();
+                bfs.pop();
                 ans.push_back(node);
+                
                 for(auto x:adj[node])
                 {
-                    indegeree[x]--;
-                    if(indegeree[x]==1)
-                        q.push(x);
+                    ind[x]--;
+                    if(ind[x]==1)
+                        bfs.push(x);
                 }
             }
         }
-        
-        if(n==1)
-            ans.push_back(0);
-        
         return ans;
-        
-//         vector<int>ans(n);
-//         for(int i=0;i<n;i++)
-//         {
-//             queue<pair<int,int>> bfs;
-//          vector<int>vis(n,0);
-            
-//             bfs.push(make_pair(i,0));
-//             int h=0;
-//             vis[i]=1;
-            
-//             while(!bfs.empty())
-//             {
-//                pair<int,int>temp=bfs.front();
-//                 bfs.pop();
-                
-//                 int node=temp.first;
-//                 int hei=temp.second;
-//                 vis[node]=1;
-                
-//                 for(auto x:adj[node])
-//                 {
-//                     if(!vis[x])
-//                     {
-//                         bfs.push(make_pair(x,hei+1));
-//                     }
-//                 }
-                
-//                 h=max(h,hei);
-                
-//             }            
-//                ans[i]=h;  
-//         }
-        
-//         unordered_map<int,vector<int>>mp;
-        
-//         int mn=INT_MAX;
-//         for(int i=0;i<ans.size();i++)
-//         {
-//             mn=min(mn,ans[i]);
-//             mp[ans[i]].push_back(i);
-//         }
-        
-//         vector<int> a;
-//         a=mp[mn];
-        
-//         return a;
-        
-        
     }
 };
