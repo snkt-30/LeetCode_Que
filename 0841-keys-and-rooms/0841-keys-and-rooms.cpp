65435674b9a0e197853a7936bac668191ba87605
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-14 13:58:31 +0530

    Time: 3 ms (99.73%), Space: 10.5 MB (55.60%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,15 +1,40 @@
-class Solution {
-public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_set<int> visited;
-        queue<int> to_visit;
-        to_visit.push(0);
-        while(!to_visit.empty()) {
-            int curr = to_visit.front();
-            to_visit.pop();
-            visited.insert(curr);
-            for (int k : rooms[curr]) if (visited.find(k) == visited.end()) to_visit.push(k);
+class Solution
+{
+    public:
+        bool canVisitAllRooms(vector<vector < int>> &rooms)
+        {
+
+            int n = rooms.size();
+
+             vector<int> visited_rooms(n, 0);
+
+            visited_rooms[0] = true;
+
+            queue<int> bfs;
+
+            bfs.push(0);
+
+            while (!bfs.empty())
+            {
+                int node = bfs.front();
+                bfs.pop();
+
+                for (auto x: rooms[node])
+                {
+                    if (visited_rooms[x] == false)
+                    {
+                        visited_rooms[x] = true;
+                        bfs.push(x);
+                    }
+                }
+            }
+            
+            for(int i=0;i<n;i++)
+            {
+                if(visited_rooms[i]==0)
+                    return false;
+            }
+            
+            return true;
         }
-        return visited.size() == rooms.size();
-    }
 };
