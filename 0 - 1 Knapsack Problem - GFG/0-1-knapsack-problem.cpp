b62572028a472c3d5421c34a6711e3e016581109
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-02-24 01:29:10 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,44 +7,64 @@ using namespace std;
 class Solution
 {
     
-    private:
+    // private:
     
-    int helper_knapsack(int ind ,int wt[],int val[],int n,int W)
-    {
-        if(ind==n)
-        {
-            if(wt[ind]<=W)
-            {
-                return val[ind];
-            }
-            return 0;
-        }
+    // int helper_knapsack(int ind ,int wt[],int val[],int n,int W)
+    // {
+    //     if(ind==n)
+    //     {
+    //         if(wt[ind]<=W)
+    //         {
+    //             return val[ind];
+    //         }
+    //         return 0;
+    //     }
         
-        if(dp[ind][W]!=-1)
-          return dp[ind][W];
+    //     if(dp[ind][W]!=-1)
+    //       return dp[ind][W];
         
-        int pick=0;
+    //     int pick=0;
         
-        if(wt[ind]<=W)
-           pick=val[ind]+helper_knapsack(ind+1,wt,val,n,W-wt[ind]);
+    //     if(wt[ind]<=W)
+    //       pick=val[ind]+helper_knapsack(ind+1,wt,val,n,W-wt[ind]);
            
-         int not_pick=helper_knapsack(ind+1,wt,val,n,W);
+    //      int not_pick=helper_knapsack(ind+1,wt,val,n,W);
          
-         return dp[ind][W]= max(pick,not_pick);
-    }
+    //      return dp[ind][W]= max(pick,not_pick);
+    // }
     public:
     
-    int dp[1002][1002];
+    // int dp[1002][1002];
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
        
-       memset(dp,-1,sizeof(dp));
-       
-       int ans=helper_knapsack(0,wt,val,n-1,W);
-       
-       return ans;
+     vector<vector<int>> dp(n,vector<int>(W+1,0));
+     
+     for(int i=wt[0];i<=W;i++)
+     {
+         dp[0][i]=val[0];
+     }
+     
+     for(int i=1;i<n;i++)
+     {
+         for(int j=0;j<=W;j++)
+         {
+             int not_take=0+dp[i-1][j];
+             
+             int take=INT_MIN;
+             
+             if(wt[i]<=j)
+             {
+                 take=val[i]+dp[i-1][j-wt[i]];
+             }
+             
+             dp[i][j]=max(not_take,take);
+         }
+     }
+     
+    return dp[n-1][W];
     }
 };
 
