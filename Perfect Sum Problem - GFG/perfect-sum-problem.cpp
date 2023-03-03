37f6aaf24c7ab1fa0cec5d4158c43cfb5de2abbb
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-03-03 17:16:53 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -7,49 +7,13 @@ using namespace std;
 #define mod 1000000007
 
 class Solution{
-    
-    private:
-    
-     int helper(int a[],int n,int ind,int sum, vector<vector<int>>&dp)
-     {
-         if(ind==n)
-         {
-             if(sum==0)
-                return 1;
-              
-             return 0;
-         }
-         
-         if(dp[ind][sum]!=-1)
-           return dp[ind][sum];
-         
-         int take=0;
-         
-         if(a[ind]<=sum)
-           take=helper(a,n,ind+1,sum-a[ind],dp)%mod;
-           
-          int not_take=helper(a,n,ind+1,sum,dp)%mod;
-          
-          return dp[ind][sum]=((take%mod)+(not_take%mod))%mod;
-     }
+
 	public:
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
         
-        // int cnt=0;
-        
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
-        
-        // for(int i=0;i<=n;i++)
-        // {
-        //     for(int j=0;j<=sum;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
+        vector<vector<int>> dp(n+1,vector<int>(sum+1));
         
         for(int i=0;i<=sum;i++)
         {
@@ -69,32 +33,16 @@ class Solution{
                 
                 if(arr[i-1]<=j)
                 {
-                    take=dp[i-1][j-arr[i-1]]%mod;
+                    take=dp[i-1][j-arr[i-1]];
                 }
                 
-                int not_take=dp[i-1][j]%mod;
+                int not_take=dp[i-1][j];
                 
-                dp[i][j]=((take%mod)+(not_take%mod))%mod;
-                
-                
+                dp[i][j]=(take%mod+not_take%mod)%mod;
             }
         }
         
-        // int ans= helper(arr,n,0,sum,dp);
-        
-        int ans=dp[n][sum];
-         
-         
-        //   for(int i=0;i<=n;i++)
-        // {
-        //     for(int j=0;j<=sum;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
-        return ans;
+        return dp[n][sum];
 	}
 	  
 };
