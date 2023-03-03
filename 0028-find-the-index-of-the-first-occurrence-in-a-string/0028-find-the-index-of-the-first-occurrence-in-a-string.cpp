7a3390d6a77eba28311843d84b9f4a66757cc752
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-03-03 23:45:48 +0530

    Time: 3 ms (43.09%), Space: 7.4 MB (11.62%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,61 @@
+class Solution
+{
+    private:
+    
+    bool check(int l,int r,string h,string n)
+    {
+        int ind=0;
+        for(int i=l;i<=r;i++)
+        {
+            if(n[ind]!=h[i])
+                return false;
+            
+            ++ind;
+        }
+        return true;
+    }
+
+    public:
+        int strStr(string h, string n)
+        {
+            int ans = -1;
+
+            if (h.length() < n.length())
+                return -1;
+
+            // vector<int> need(26, 0), track(26, 0);
+            
+            if(h.length()==n.length())
+            {
+                if(h==n)
+                    return 0;
+                return -1;
+            }
+
+            // for (int i = 0; i < n.length(); i++)
+            // {
+            //     need[n[i] - 'a']++;
+            // }
+
+            int l = 0, r = 0;
+
+            while (r < h.length())
+            {
+
+                if (r - l + 1 < n.length())
+                    r++;
+                else if (r - l + 1 == n.length())
+                {
+                    if (check(l,r,h,n))
+                    {
+                        ans = l;
+                        break;
+                    }
+                    l++;
+                    r++;
+                }
+            }
+
+            return ans;
+        }
+};
