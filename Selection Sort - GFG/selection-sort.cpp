9b05a81b89f2e2bf55f77363f50ce4494806a00e
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-03-24 00:25:13 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.cpp
+++ Selection Sort - GFG/selection-sort.cpp
@@ -18,30 +18,27 @@ class Solution
         // code here such that selectionSort() sorts arr[]
         
         // int n = sizeof(arr)/sizeof(arr[0]);
-        
-        int ind=i;
-        
-        for(int j=i;j<n;j++)
-        {
-            if(arr[j]<arr[ind])
-              ind=j;
-        }
-        
-        return ind;
+        return 0;
     }
      
     void selectionSort(int arr[], int n)
     {
+        
        //code here
+       
+       int sz=select(arr,0,4);
        
        for(int i=0;i<n-1;i++)
        {
+           int mn=i;
            
-           int sm=arr[i];
+           for(int j=i;j<n;j++)
+           {
+               if(arr[j]<arr[mn])
+                 mn=j;
+           }
            
-           int ind=select(arr,i,n);
-           
-           swap(arr[ind],arr[i]);
+           swap(arr[i],arr[mn]);
        }
        
        
