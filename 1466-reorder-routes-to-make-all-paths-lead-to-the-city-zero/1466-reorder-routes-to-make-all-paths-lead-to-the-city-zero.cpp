1d2f848514600bfe931dd60dd42779d620295010
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-03-24 13:26:08 +0530

    Time: 630 ms (23.84%), Space: 130.2 MB (29.93%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,12 +1,34 @@
+#define N 1e5+10
+
 class Solution {
+    
+    private:
+    
+    void dfs(int node,int &cnt)
+    {
+        vis[node]=true;
+        
+        for(auto x:adj[node])
+        {
+            if(!vis[x] and st.count({node,x}))
+            {
+                cnt++;
+            }
+            
+            if(!vis[x])
+                dfs(x,cnt);
+        }
+    }
+   
 public:
+      set<pair<int,int>> st;
+      bool vis[50005];
+      vector<int>adj[50005];       
     int minReorder(int n, vector<vector<int>>& c) {
         
-       set<pair<int,int>> st;
-        vector<int>vis(n,0);
-        vector<int>adj[n];
         
-        for(auto x:c)
+        int cnt=0;
+          for(auto x:c)
         {
             int u=x[0];
             int v=x[1];
@@ -17,11 +39,27 @@ public:
             st.insert(make_pair(u,v));
         }
         
+        // vis.resize(N);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+                dfs(i,cnt);
+        }
+        
+        return cnt;
+        
+//        set<pair<int,int>> st;
+//         vector<int>vis(n,0);
+//         vector<int>adj[n];
+        
+      
+        
         queue<int> bfs;
         
         bfs.push(0);
         
-        int cnt=0;
+        // int cnt=0;
         
         while(!bfs.empty())
         {
