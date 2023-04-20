24e3c0d27412a8603aefdce714d23989d132258f
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-04-20 12:36:33 +0530

    Time: 0 ms (100.00%), Space: 17.4 MB (25.97%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -9,30 +9,86 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#define mk make_pair
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-     queue<pair<TreeNode*,int>>q;
-        q.push({root,0});
-        long ans = 0;
-        while(!q.empty()){
-            int size = q.size();
-            long l = 0;
-            long r = 0;
-            for(int i=0; i<size; i++){
-                auto top = q.front();
-                q.pop();
-                TreeNode* node = top.first;
-                long idx = top.second;
-                if(i == 0) l = idx;
-                if(i == size-1) r = idx;
-                if(node->left) q.push({node->left,idx*2+1});
-                if(node->right) q.push({node->right,idx*2+2});
+        
+        if(!root)
+            return 0;
+        
+        if(!root->left and !root->right)
+            return 1;
+        
+        
+        queue<pair<TreeNode*,int>> bfs;
+        
+        bfs.push(mk(root,1));
+        
+        long long ans=1;
+        
+        while(!bfs.empty())
+        {
+           int n=bfs.size();
+            
+            long long l=0,r=0;
+            
+            for(int i=0;i<n;i++)
+            {
+                auto x=bfs.front();
+                bfs.pop();
+                
+                TreeNode* node=x.first;
+                long long ind=x.second;
+                
+                if(node->left)
+                {
+                    long long  l_ind=ind*1LL*2;
+                    
+                    if(l==0)
+                    {
+                        l=l_ind;
+                    }
+                    else
+                    {
+                        r=l_ind;
+                    }
+                    
+                    bfs.push(mk(node->left,l_ind));
+                }
+                
+                if(node->right)
+                {
+                    long long r_ind=ind*1LL*2+1;
+                    
+                    if(l==0)
+                    {
+                        l=r_ind;
+                    }
+                    else
+                    {
+                        r=r_ind;
+                    }
+                    
+                    bfs.push(mk(node->right,r_ind));
+                }
             }
-            ans = max(ans,r-l+1);
+            
+            long long temp=0;
+            
+            if(r==0 or l==0)
+            {
+                temp=1;
+            }
+            else
+            {
+                temp=r-l+1;
+            }
+            ans=max(ans,temp);
         }
+        
         return ans;
-        
-        
     }
 };
