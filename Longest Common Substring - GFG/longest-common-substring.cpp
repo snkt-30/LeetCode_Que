39069760b08b779500bc1fcbcaadcdaa78cecc23
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-04-26 13:21:37 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -4,11 +4,61 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
+    
+    private:
+    
+    int lcs(string a,string b,int n,int m)
+    {
+       if(n==0 or m==0)
+        {
+            return 0;
+        }
+        
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
+        
+        int match;
+        
+        if(a[n-1]==b[m-1])
+        {
+            match=1+lcs(a,b,n-1,m-1);
+        }
+        else
+        {
+            match=0;
+        }
+        
+        lcs(a,b,n-1,m);
+        lcs(a,b,n,m-1);
+        
+        return dp[n][m]=match;
+        
+    }
     public:
+    
+    int dp[1001][1001];
     
     int longestCommonSubstr (string a, string b, int n, int m)
     {
         // your code here
+        
+        memset(dp,-1,sizeof(dp));
+        
+        // int ans=0;
+        
+        //  lcs(a,b,n,m);
+         
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=m;j++)
+        //     {
+        //         ans=max(ans,dp[i][j]);
+        //     }
+        // }
+        
+        // return ans;
         
         int dp[n+1][m+1];
         
@@ -40,15 +90,6 @@ class Solution{
                 mx=max(mx,dp[i][j]);
             }
         }
-        
-        // for(int i=0;i<=n;i++)
-        // {
-        //     for(int j=0;j<=m;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         
         return mx;
     }
