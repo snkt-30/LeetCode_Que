9b95a91a87c48311beb99a0e3c8f80300be8b956
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-05-11 16:53:59 +0530

    Time: 16 ms (78.63%), Space: 13 MB (63.96%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& a, vector<int>& b) {
+        
+        int n=a.size();
+        int m=b.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=0;
+        }
+        
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=0;
+        }
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(a[i-1]==b[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        
+        }
+        
+        return dp[n][m];
+    }
+    
+};
