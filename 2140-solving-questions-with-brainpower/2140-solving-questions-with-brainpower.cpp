f8f06d3c65e44c9f83b8596917c1c00cac1254b3
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-05-13 00:01:37 +0530

    Time: 378 ms (57.53%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,31 +1,31 @@
 class Solution {
     
-    private:
+//     private:
     
-    long long helper(int ind,vector<vector<int>>& a, vector<long long>&dp,int n)
-    {
-        if(ind>=n)
-        {
-            return 0;
-        }
+//     long long helper(int ind,vector<vector<int>>& a, vector<long long>&dp,int n)
+//     {
+//         if(ind>=n)
+//         {
+//             return 0;
+//         }
         
         
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
+//         if(dp[ind]!=-1)
+//         {
+//             return dp[ind];
+//         }
          
-        long long dis=a[ind][1];
-        long long take=a[ind][0]+helper(ind+dis+1,a,dp,n);
+//         long long dis=a[ind][1];
+//         long long take=a[ind][0]+helper(ind+dis+1,a,dp,n);
         
-        long long not_take=0+helper(ind+1,a,dp,n);
+//         long long not_take=0+helper(ind+1,a,dp,n);
         
-        dp[ind]=max(take,not_take);
+//         dp[ind]=max(take,not_take);
         
-        return dp[ind];
+//         return dp[ind];
         
         
-    }
+//     }
 public:
     long long mostPoints(vector<vector<int>>& a) {
         
