6b0f861a0f748128b9c52651d8a608f4f4a32dd4
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-07-06 09:07:23 +0530

    Time: 44 ms (51.97%), Space: 28.2 MB (71.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,60 +1,37 @@
 class Solution {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
-   /*  
-     time Complexity : O(N)
-    int len = 100001;
-
-	int st_ind = 0, end_ind = 0;
-
-	int sum = 0;
-
-	while (end_ind < nums.size())
-	{
-		sum += nums[end_ind];
-
-		if (sum < target)
-			end_ind++;
-		else if (sum >= target)
-		{
-			while (sum >= target and st_ind <= end_ind)
-			{
-				int curr = end_ind - st_ind + 1;
-				if (len > curr)
-				{
-					len = curr;
-				}
-				sum -= nums[st_ind];
-				st_ind++;
-			}
-			end_ind++;
-		}
-	}
-
-	return len == 100001 ? 0 : len;
-    
-    */
+    int minSubArrayLen(int t, vector<int>& a) {
         
-  // Time Complexity O(log(N))
         
-        int n=nums.size();
+        int len=0;
         
-        vector<int> v(n+1,0);
+        int l=0,r=0;
         
-        int len=INT_MAX;
+        int n=a.size();
         
-        for(int i=1;i<=n;i++)
+        int ans=INT_MAX;
+        
+        int sum=0;
+        while(r<n)
         {
-            v[i]=v[i-1]+nums[i-1];
-        }
-        
-        for(int i=n;i>=0 and v[i]>=s;i--)
-        {
-            int ind=upper_bound(v.begin(),v.end(),v[i]-s)-v.begin();
-            len=min(len,i-ind+1);
+            sum+=a[r];
             
+            
+            if(sum>=t)
+            {                
+                while(sum>=t and l<n and l<=r)
+                {
+                    ans=min(ans,r-l+1);
+                    sum-=a[l];                    
+                    l++;
+                }
+                
+            }
+            r++;
         }
         
-        return len==INT_MAX?0:len;
+       return ans==INT_MAX?0:ans;
+        
+        
     }
 };
