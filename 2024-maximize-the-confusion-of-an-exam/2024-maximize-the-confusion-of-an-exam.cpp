27b00a51a6ae9a6d9c6795e511c06607dbca59f9
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-07-07 22:48:59 +0530

    Time: 22 ms (90.53%), Space: 10 MB (59.24%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        
+        
+        int n=s.size();
+        
+        if(n==1)
+            return 1;
+        
+        if(n==2 and s[0]==s[1])
+            return 2;
+        
+        
+        int r=0,l=0;
+        
+        int t_cnt=0,f_cnt=0,mn=INT_MAX;
+        
+        int ans=0;
+        while(r<n)
+        {
+            if(s[r]=='T')
+            {
+                t_cnt++;
+            }
+            else
+            {
+                f_cnt++;
+            }
+            
+            mn=min(t_cnt,f_cnt);
+            
+            if(mn>k)
+            {
+                while(mn>k and l<n and l<=r)
+                {
+                    if(s[l]=='T')
+                    {
+                        t_cnt--;
+                    }
+                    else
+                    {
+                        f_cnt--;
+                    }
+                    
+                    mn=min(t_cnt,f_cnt);
+                    l++;
+                }
+            }
+            ans=max(ans,r-l+1);
+            r++;
+        }
+        
+        return ans;
+        
+    }
+};
