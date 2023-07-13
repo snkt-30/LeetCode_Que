ad9b9f22069cc05f1808b4ae004308f1ce0709d8
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-07-13 11:02:15 +0530

    Time: 26 ms (51.44%), Space: 13.8 MB (52.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,56 +1,58 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& pr) {
-        
-        // its simple let considere the pair as u and v where there is directed edge form u to v
-        // now as we simplified this will form a directed graph now as the ques ask finish course 
-        // means that can we form a  topological sort or not. so the problem break down into that 
-        // the graph contain cycle or not . if it contain cycle there the topological sort is not 
-        // is not possible bcoz the element is not conitain whole vertex. so just return if 
-        // if size of topo array is not equal to numCourses the false else true;
+    bool canFinish(int nc, vector<vector<int>>& pr) {
         
         
-     vector<int> adj[n];
-      vector<int> ind(n,0);
+        
+        vector<int>indegree(nc);
+        
+        vector<int> adj[nc];
         
         for(auto x:pr)
         {
             int u=x[0];
             int v=x[1];
             
-            ind[v]++;
-            
             adj[u].push_back(v);
+            
+            indegree[v]++;
         }
         
-        queue<int> bfs;
+        queue<int>bfs;
         
-        for(int i=0;i<n;i++)
+        vector<int> courses_can_take;
+        
+        for(int i=0;i<nc;i++)
         {
-            if(ind[i]==0)
+            if(indegree[i]==0)
+            {
                 bfs.push(i);
+            }
         }
-        
-        int cnt=0;
         
         while(!bfs.empty())
         {
             int node=bfs.front();
             bfs.pop();
             
-            cnt++;
+            courses_can_take.push_back(node);
             
             for(auto x:adj[node])
             {
-                ind[x]--;
+                indegree[x]--;
                 
-                if(ind[x]==0)
+                if(indegree[x]==0)
+                {
                     bfs.push(x);
+                }
             }
         }
         
-        return cnt==n;
+        if(courses_can_take.size()!=nc)
+        {
+            return false;
+        }
         
-        
+        return true;
     }
 };
