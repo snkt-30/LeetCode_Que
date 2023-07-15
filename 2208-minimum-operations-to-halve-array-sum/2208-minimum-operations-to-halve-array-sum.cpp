0615b2938d84301b8c08d1ee9adc66b14f5156e8
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-07-15 21:30:45 +0530

    Time: 1133 ms (5.10%), Space: 89.5 MB (8.20%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        
+        
+        
+        double sum=0.0;
+        
+        priority_queue<double> pq;
+        
+        
+        
+        for(auto x:nums)
+        {
+            pq.push(x);
+            sum+=x;
+        }
+        
+        double half=sum/2.0;
+        
+        double reduced=0.0;
+        
+        int oper=0;
+        
+        
+        cout<<sum<<" "<<half<<endl;
+        while(!pq.empty() and half>reduced)
+        {
+            
+            double val=pq.top();
+            pq.pop();
+            
+            double h=val/2.0;
+            
+            reduced+=h;
+            pq.push(h);
+            oper++;
+            cout<<reduced<<endl;
+        }
+        
+        return oper;
+    }
+};
