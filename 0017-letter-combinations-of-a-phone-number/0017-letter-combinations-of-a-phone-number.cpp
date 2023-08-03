01c82d2f53ff553927533a9f506de1762b6f9187
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-08-03 07:10:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,5 @@
 class Solution {
-     unordered_map<int,string>mp;
-    
+    vector<string> mp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
      vector<string>ans;
     
     void gen_combo(int ind,string ds,string d)
@@ -29,14 +28,14 @@ public:
             return {};
         }
         
-        mp[2]="abc";
-        mp[3]="def";
-        mp[4]="ghi";
-        mp[5]="jkl";
-        mp[6]="mno";
-        mp[7]="pqrs";
-        mp[8]="tuv";
-        mp[9]="wxyz";
+//         mp[2]="abc";
+//         mp[3]="def";
+//         mp[4]="ghi";
+//         mp[5]="jkl";
+//         mp[6]="mno";
+//         mp[7]="pqrs";
+//         mp[8]="tuv";
+//         mp[9]="wxyz";
         
 //         vector<string> st;
         
