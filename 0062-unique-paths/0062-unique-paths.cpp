fab48cf32008a0faca08d8983b393a76784a294b
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-09-03 09:34:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,25 @@
 class Solution {
     
-    int ans(int row,int col,int dp[][101])
-    {
+//     int ans(int row,int col,int dp[][101])
+//     {
         
-        if(row<0 or col<0)
-            return 0;
-        if(row==0 and  col==0)
-        {
-            return 1;
-        }
+//         if(row<0 or col<0)
+//             return 0;
+//         if(row==0 and  col==0)
+//         {
+//             return 1;
+//         }
         
-        if(dp[row][col]!=-1)
-            return dp[row][col];
-          int up=ans(row-1,col,dp);
+//         if(dp[row][col]!=-1)
+//             return dp[row][col];
+//           int up=ans(row-1,col,dp);
         
-           int left=ans(row,col-1,dp);
+//            int left=ans(row,col-1,dp);
         
-       return  dp[row][col]=up+left;
+//        return  dp[row][col]=up+left;
         
         
-    }
+//     }
 public:
     int uniquePaths(int m, int n) {
         
@@ -46,6 +46,6 @@ public:
         
         return dp[m-1][n-1];
         
-        return ans(m-1,n-1,dp);
+        // return ans(m-1,n-1,dp);
     }
 };
