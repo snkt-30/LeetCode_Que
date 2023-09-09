35c56000809766f3556c1913461f01e29106c10e
Sanket Khonde <84696262+snkt-30@users.noreply.github.com>
2023-09-09 23:40:10 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -2,43 +2,45 @@ class Solution {
     
     private:
     
-    vector<vector<int>>dp;
+    vector<int>dp;
     
-    int helper(int ind,int sum,vector<int>&a,int t)
+    int helper(int sum,vector<int>&a,int t)
     {
         if(sum>t)
         {
             return 0;
         }
         
-        if(ind>=a.size())
-            return 0;
+//         if(ind>=a.size())
+//             return 0;
         
         if(sum==t)
         {
             return 1;
         }
         
-        if(dp[ind][sum]!=-1)
+        if(dp[sum]!=-1)
         {
-           return dp[ind][sum];
+           return dp[sum];
         }
         int res=0;
         
         for(int i=0;i<a.size();i++)
         {
-            res+=helper(i,sum+a[i],a,t);
+            if(sum+a[i]<=t){
+            res+=helper(sum+a[i],a,t);
+            }
         }
         
-        return dp[ind][sum]=res;
+        return dp[sum]=res;
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         
-        dp.resize(500,vector<int>(200010,-1));
+        dp.resize(1001,-1);
         
         // memset(dp,-1,sizeof(dp));
         
-        return helper(0,0,nums,target);
+        return helper(0,nums,target);
     }
 };
